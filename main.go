1fce2c494efb8ee680a503abecf5b5707134b5d4
zleodai <awsomeleodai@gmail.com>
2024-05-28 18:53:59 -0700

    converted json converter into module

--- main.go
+++ main.go
@@ -1,74 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"modules/pointCloudDecoder"
-	"os"
 )
 
 var jsonFilePath string = "./example_files/SeaverSignPCJSON.json"
+var convertedJsonFileName string = "pointCloud.JSON"
 
 func main() {
-	file, errs := os.Create("pointCloud.JSON")
-	if errs != nil {
-		fmt.Println("Failed to create file:", errs)
-		return
-	}
-	defer file.Close()
+	pointCloudDecoder.GenerateFloatJson(jsonFilePath, convertedJsonFileName)
 
-	var data pointCloudDecoder.PointCloud = pointCloudDecoder.DecodeFromPath(jsonFilePath)
-
-	_, errs = file.WriteString("{\"points\": \n	[")
-	if errs != nil {
-		fmt.Println("Failed to write to file:", errs)
-		return
-	}
-
-	for index, structure := range data.Structures {
-		_, errs = file.WriteString("\n		{")
-		if errs != nil {
-			fmt.Println("Failed to write to file:", errs)
-			return
-		}
-		for index, x := range structure.X {
-			if index == 0 {
-				_, errs = file.WriteString("\n			\"x\": " + x + ",")
-				if errs != nil {
-					fmt.Println("Failed to write to file:", errs)
-					return
-				}
-			} else if index == 1 {
-				_, errs = file.WriteString("\n			\"y\": " + x + ",")
-				if errs != nil {
-					fmt.Println("Failed to write to file:", errs)
-					return
-				}
-			} else {
-				_, errs = file.WriteString("\n			\"z\": " + x)
-				if errs != nil {
-					fmt.Println("Failed to write to file:", errs)
-					return
-				}
-			}
-		}
-		if index != len(data.Structures)-1 {
-			_, errs = file.WriteString("\n		}, ")
-			if errs != nil {
-				fmt.Println("Failed to write to file:", errs)
-				return
-			}
-		} else {
-			_, errs = file.WriteString("\n		}")
-			if errs != nil {
-				fmt.Println("Failed to write to file:", errs)
-				return
-			}
-		}
-	}
-
-	_, errs = file.WriteString("\n	]\n}")
-	if errs != nil {
-		fmt.Println("Failed to write to file:", errs)
-		return
-	}
 }
--- modules/pointCloudDecoder/pointCloudDecoder.go
+++ modules/pointCloudDecoder/pointCloudDecoder.go
@@ -71,7 +71,7 @@ type Observation struct {
 	Scale         string   `json:"scale"`
 }
 
-func DecodeFromPath(jsonFilePath string) PointCloud {
+func DecodeFromRawJsonFromPath(jsonFilePath string) PointCloud {
 	jsonFile, err := os.ReadFile(jsonFilePath)
 	check(err)
 
@@ -80,4 +80,50 @@ func DecodeFromPath(jsonFilePath string) PointCloud {
 	json.Unmarshal(jsonFile, &pointCloudData)
 
 	return pointCloudData
+}
+
+func GenerateFloatJson(jsonFilePath string, jsonFileName string) string {
+	file, errs := os.Create(jsonFileName)
+	if errs != nil {
+		panic("Failed to write to file:" + errs.Error())
+	}
+	defer file.Close()
+
+	var data PointCloud = DecodeFromRawJsonFromPath(jsonFilePath)
+
+	_, errs = file.WriteString("{\"points\": \n	[")
+	check(errs)
+
+	for index, structure := range data.Structures {
+		_, errs = file.WriteString("\n		{")
+		check(errs)
+		for index, x := range structure.X {
+			switch checker := index; checker {
+			case 0:
+				_, errs = file.WriteString("\n			\"x\": " + x + ",")
+				check(errs)
+			case 1:
+				_, errs = file.WriteString("\n			\"y\": " + x + ",")
+				check(errs)
+			case 2:
+				_, errs = file.WriteString("\n			\"z\": " + x)
+				check(errs)
+			default:
+				panic("More than 3 cordinates found cannot calculate 4d or greater coords")
+			}
+		}
+		if index != len(data.Structures)-1 {
+			_, errs = file.WriteString("\n		}, ")
+			check(errs)
+		} else {
+			_, errs = file.WriteString("\n		}")
+			check(errs)
+		}
+	}
+
+	_, errs = file.WriteString("\n	]\n}")
+	check(errs)
+
+	var outputPath string = "./" + jsonFileName
+	return outputPath
 }
