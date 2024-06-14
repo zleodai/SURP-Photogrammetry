97555b97cef5f7038a775471b06bd03031bcbd77
zleodai <awsomeleodai@gmail.com>
2024-06-13 21:52:26 -0700

    Reverted voxelMesher GenerateVoxelJson function

--- main.go
+++ main.go
@@ -11,15 +11,16 @@ import (
 	"strconv"
 )
 
-// var jsonFilePath string = "./example_files/footballPCJSON.json"
-// var convertedJsonFileName string = "pointCloud.JSON"
-var convertedJsonFilePath string = "./pointCloud.JSON"
+var jsonFilePath string = "./example_files/footballPCJSON.json"
+var convertedJsonFileName string = "pointCloud.JSON"
+// var convertedJsonFilePath string = "./pointCloud.JSON"
 
-var defaultVoxelSize float64 = 0.01
+var defaultVoxelSize float64 = 0.05
 
 func main() {
 	// commented line for going from meshroom json data to a cleaned up version this program uses
-	// var convertedJsonFilePath string = pointCloudDecoder.GenerateFloatJson(jsonFilePath, convertedJsonFileName)
+	var convertedJsonFilePath string = pointCloudDecoder.GenerateFloatJson(jsonFilePath, convertedJsonFileName)
+	runtime.GC()
 	var pointData pointCloudDecoder.PointData = pointCloudDecoder.DecodeFromFloatJsonFromPath(convertedJsonFilePath)
 	fmt.Printf("Running Program\n%s Points Loaded\n", strconv.Itoa(len(pointData.Points)))
 
--- modules/pointCloudDecoder/pointCloudDecoder.go
+++ modules/pointCloudDecoder/pointCloudDecoder.go
@@ -108,11 +108,42 @@ func GenerateFloatJson(jsonFilePath string, jsonFileName string) string {
 	if errs != nil {
 		panic("Failed to write to file:" + errs.Error())
 	}
-	
+	defer file.Close()
+
 	var data PointCloud = DecodeFromRawJsonFromPath(jsonFilePath)
 
-	enc := json.NewEncoder(file)
-	enc.Encode(data)
+	_, errs = file.WriteString("{\"points\": \n	[")
+	check(errs)
+
+	for index, structure := range data.Structures {
+		_, errs = file.WriteString("\n		{")
+		check(errs)
+		for index, x := range structure.X {
+			switch checker := index; checker {
+			case 0:
+				_, errs = file.WriteString("\n			\"x\": " + x + ",")
+				check(errs)
+			case 1:
+				_, errs = file.WriteString("\n			\"y\": " + x + ",")
+				check(errs)
+			case 2:
+				_, errs = file.WriteString("\n			\"z\": " + x)
+				check(errs)
+			default:
+				panic("More than 3 cordinates found cannot calculate 4d or greater coords")
+			}
+		}
+		if index != len(data.Structures)-1 {
+			_, errs = file.WriteString("\n		}, ")
+			check(errs)
+		} else {
+			_, errs = file.WriteString("\n		}")
+			check(errs)
+		}
+	}
+
+	_, errs = file.WriteString("\n	]\n}")
+	check(errs)
 
 	var outputPath string = "./" + jsonFileName
 	return outputPath
